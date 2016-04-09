cd70422957ff9ac3b3821460b0ea360a7c3d74c6
itaydafna <itaydafna@gmail.com>
2016-04-09 20:48:02 +0300

    adding my first ever 'to do' list

---
+++ script.js
@@ -0,0 +1,35 @@
+function addAction() {
+  var actionItem = document.getElementById("action").value,
+    ol = document.getElementById("list"),
+    li = document.createElement("li");
+  li.appendChild(document.createTextNode(actionItem))
+  ol.appendChild(li);
+}
+
+var button = document.getElementById("submit");
+
+button.addEventListener("click", addAction);
+
+document.addEventListener("keydown", function(event) {
+  if (event.keyCode === 13) {
+    addAction();
+  } else {
+    return false
+  }
+})
+
+// upon click - willl cross-out li, or delete it if it's already crossed
+document.addEventListener("click", function(e) {
+  var item = e.target;
+  if (item.nodeName.toLowerCase() === "li") {
+    if (item.className === "crossed-out") {
+    	item.remove();
+    } 
+    else {
+      item.className +="crossed-out";
+    }
+  } 
+  else {
+    return false
+  };
+})
