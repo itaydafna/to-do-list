aee2679588ca790b3048e74bd4f1c5114ac89399
Itay Dafna <itaydafna@gmail.com>
2016-04-10 13:56:13 +0300

    document.getElementById("action").value="";

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function addAction() {
     li = document.createElement("li");
   li.appendChild(document.createTextNode(actionItem))
   ol.appendChild(li);
-  actionItem="";
+  document.getElementById("action").value="";
 }
 
 var button = document.getElementById("submit");
